e5990e69f5f26d612344ca1152111f129f0aa9be
Beachyshells <151598119+Beachyshells@users.noreply.github.com>
2025-10-11 23:57:07 -0400

    added the header for my dictionary App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import logo from "./images/CodeSynapseLogo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    // Header Hero
+    <div className="App App-header d-flex   justify-content-center">
+      <div className="Header-content d-flex align-items-center">
+        <img className="Logo" alt="CodeSynapse Logo" src={logo} />
+        <div className="Heading">
+          <header className="Title">Code Synapse</header>
+          <p className="sub-title">Your Intelligent Developer Dictionary</p>
+        </div>
+        <div className="icon">
+          <p>📒</p>
+        </div>
+      </div>
     </div>
   );
 }
