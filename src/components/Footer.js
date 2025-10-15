22fdf011172a124678044ea137927da118405775
Beachyshells <151598119+Beachyshells@users.noreply.github.com>
2025-10-15 15:18:49 -0400

    added author to footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,18 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <footer>hey footer</footer>
+    <div className="Footer">
+      <footer>
+        <div>
+          This project was coded by 🐞
+          <a href="https://www.shecodes.io/graduates/169424-michelle-durham">
+            Michelle Durham
+          </a>{" "}
+          is open sourced on{" "}
+          <a href="https://github.com/Beachyshells/code-synapse">GitHub</a>and
+          hosted on <a href="https://codesynapse.netlify.app/">Netlify</a>
+        </div>
+      </footer>
     </div>
   );
 }
