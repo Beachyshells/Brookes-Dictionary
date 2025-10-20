eceac1451ac34b2c1136d45f441b9f79dd8b51c1
Beachyshells <151598119+Beachyshells@users.noreply.github.com>
2025-10-20 11:15:29 -0400

    i can not get the pexels API to work something about CORS so I have been trying to fix this

--- src/App.js
+++ src/App.js
@@ -10,13 +10,29 @@ function App() {
   const [results, setResults] = useState({});
   const [studyList, setStudyList] = useState([]);
   const [isCollectionVisible, setIsCollectionVisible] = useState(false);
-
+  const [photos, setPhotos] = useState([]);
   function toggleCollection() {
     setIsCollectionVisible(!isCollectionVisible);
   }
 
   function handleResponse(response) {
+    const word = response.data.word;
+
+    let imageApiUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=3`;
+    let imageApiKey =
+      "lMRUHUwoFSTbNFBdsucoaUi0FAKy7UHCqHrbRbdy1k2wv7EwHPu3nqRN";
+    axios
+      .get(imageApiUrl, {
+        headers: { Authorization: `Bearer ${imageApiKey}` },
+      })
+      .then(handlePexelsResponse)
+      .catch(function (error) {
+        console.error("Pexels API call failed", error);
+      });
     setResults(response.data);
+  }
+  function handlePexelsResponse(response) {
+    console.log("Pexels photo response", response.data);
   }
   const addToStudyList = () => {
     const newWord = results;
@@ -27,6 +43,7 @@ function App() {
       alert(`This word is already in your study list!`);
     }
   };
+
   function search(keyword) {
     let apiKey = "b9aaeaaf97004f2a03afob830bt63baf";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
